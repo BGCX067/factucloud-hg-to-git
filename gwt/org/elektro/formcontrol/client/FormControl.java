4b68ee043bde527a5de3f8c9e516aad74ea704eb
elektro79 <elektro79@gmail.com>
2011-07-06 12:45:45 +0200

    Añadido gwt y formcontrol

---
+++ gwt/org/elektro/formcontrol/client/FormControl.java
@@ -0,0 +1,14 @@
+package org.elektro.formcontrol.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class FormControl implements EntryPoint
+{
+
+@Override
+public void onModuleLoad()
+	{
+	Window.alert("Hello, again");
+	}
+}
